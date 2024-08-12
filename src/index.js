3b0fe7dbfee956dafb3db1a856debab90423c4e2
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-12 14:43:26 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { configureStore } from "./store";
+const store = configureStore();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
 );
 
---
+++ src/reducers/user.js
@@ -0,0 +1,13 @@
+
+
+export default function user(state = {}) {
+ 
+   
+    return {
+      id: 1,
+      name: "Abhijeet Kumar Mishra",
+      image:
+        "https://st3.depositphotos.com/10654668/14328/i/600/depositphotos_143284529-stock-photo-stylish-bearded-businessman.jpg",
+    };
+  
+  }
